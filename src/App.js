e1a228bb1532778f6e10ed674574ec5022fab8bd
mahnoormaleeka <mahnoor.intern@devsinc.com>
2023-10-18 19:33:26 +0500

    Add all files of burger-builder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BurgerBuilder from './BurgerBuilder';
+import Login from './Login';
+import Burger from './Burger';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+        
+        <Switch>
+          <Route path="/BuildBurger" component={BurgerBuilder} />
+        
+          <Route path="/Login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
